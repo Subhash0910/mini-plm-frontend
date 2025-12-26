ebd685e3650e4caebecc4fe392ec2e585229627f
Subhash <subhashvenkata1430@gmail.com>
2025-12-27 03:51:20 +0530

    Connect frontend to live backend

--- src/services/PartService.js
+++ src/services/PartService.js
@@ -1,22 +1,26 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/api;";
+const API_BASE_URL = "https://mini-plm-backend.onrender.com/api/parts";
 
 
 class PartService {
-    getAllParts() {
-        return axios.get(`${API_BASE_URL}/parts`);
-    }
-    createPart(partData) {
-        return axios.post(`${API_BASE_URL}/parts`, partData);
-    }
-    updatePart(partId, partData) {
-        return axios.put(`${API_BASE_URL}/parts/${partId}`, partData);
-    }
-    deletePart(partId) {
-        return axios.delete(`${API_BASE_URL}/parts/${partId}`);
-    }
 
+  getAllParts() {
+    return axios.get(API_BASE_URL);
+  }
+
+  createPart(part) {
+    return axios.post(API_BASE_URL, part);
+  }
+
+  updatePart(id, part) {
+    return axios.put(`${API_BASE_URL}/${id}`, part);
+  }
+
+  deletePart(id) {
+    return axios.delete(`${API_BASE_URL}/${id}`);
+  }
 }
+
 export default new PartService();
 
